ed460eb164f543dba4bc9a2768c5851bf1f80e34
Hung-I Wang <whygowe@gmail.com>
2019-02-15 14:20:29 +0800

    Update deps

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,22 @@ mod translator;
 
 use self::translator::translate;
 
-use railroad::{Diagram, Sequence};
+use railroad::{svg, Diagram, Sequence, DEFAULT_CSS};
 use regex_syntax::ast::parse::Parser;
 pub use regex_syntax::Result;
 
 pub fn generate_diagram<T: AsRef<str>>(regex: T) -> Result<Diagram<Sequence>> {
     let regex = regex.as_ref();
-    Ok(translate(regex, Parser::new().parse(regex)?))
-}
+    let mut dia = translate(regex, Parser::new().parse(regex)?);
 
+    dia.add_element(
+        svg::Element::new("style")
+            .set("type", "text/css")
+            .text(DEFAULT_CSS),
+    );
+
+    Ok(dia)
+}
 
 // #[cfg(test)]
 // mod tests {
